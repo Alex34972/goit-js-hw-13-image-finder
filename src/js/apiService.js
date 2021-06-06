0dc5fedfb679696a9a945e948d644a21811af122
Alex34972 <alex_hifi@ukr.net>
2021-06-06 16:00:43 +0300

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,12 +1,27 @@
-const url =` https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=cat&page=1&per_page=12&key=21948852-6fcb2a22057a93c1128c532ff`;
-
-function fetchImages(images) {
-    return fetch(url).then(
-      (res) => {
-        return res.json();
-      }
-    );
+export default class SearchApiImages {
+  constructor() {
+    this.searchImages = ``;
+    this.page = 1;
   }
-  export default { fetchImages };
+  fetchImages() {
+    console.log(this);
+    const url = ` https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchImages}&page=${this.page}&per_page=12&key=21948852-6fcb2a22057a93c1128c532ff`;
+    return fetch(url)
+      .then((r) => r.json())
+      .then((data) => {
+        this.page += 1;
+        return data.hits;
+      });
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchImages;
+  }
+  set query(newImages) {
+    this.searchImages = newImages;
+  }
+}
 
-  //Your API key: 21948852-6fcb2a22057a93c1128c532ff
+//Your API key: 21948852-6fcb2a22057a93c1128c532ff
