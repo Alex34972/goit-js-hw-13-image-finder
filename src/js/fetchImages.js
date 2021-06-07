58989559a7256081659993c0557379bd9c8e35df
Alex34972 <alex_hifi@ukr.net>
2021-06-07 21:39:59 +0300

    Update fetchImages.js

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,28 +1,39 @@
-
-import templGallery from '../templates/gallery-images.hbs';
-import ImagesApiService from './apiService';
+import templGallery from "../templates/gallery-images.hbs";
+import ImagesApiService from "./apiService";
 
 const refs = {
-    input:document.querySelector(".search-form"),
-    galleryMap:document.querySelector(".gallery"),
-    loadMorBtn:document.querySelector(".load-more")
-}
+  input: document.querySelector(".search-form"),
+  galleryMap: document.querySelector(".gallery"),
+  loadMorBtn: document.querySelector(`[data-action="load-more"]`),
+};
 const imagesApiService = new ImagesApiService();
 
-let debounce = require('lodash.debounce');
-refs.input.addEventListener(`input`,debounce(onSearch,1000));
-refs.loadMorBtn.addEventListener(`click`,onLoadMore);
+let debounce = require("lodash.debounce");
+refs.input.addEventListener(`input`, debounce(onSearch, 1000));
+refs.loadMorBtn.addEventListener(`click`, onLoadMore);
 
 function onSearch(e) {
-    e.preventDefault();
-    imagesApiService.query = e.target.value.trim();
-    imagesApiService.resetPage();
+  e.preventDefault();
+  clearInput();
+  imagesApiService.query = e.target.value.trim();
+  imagesApiService.resetPage();
+  if (imagesApiService.query !== ``) {
     imagesApiService.fetchImages().then(renderGallery);
-      
-    };
-function onLoadMore(){
-    imagesApiService.fetchImages().then(renderGallery)
-}    
-function renderGallery(hits){
-        refs.galleryMap.insertAdjacentHTML(`beforeend`, templGallery(hits));
-    }
+  }
+}
+function onLoadMore() {
+  if (imagesApiService.query !== ``) {
+    imagesApiService.fetchImages().then(renderGallery);
+  }
+}
+function renderGallery(hits) {
+  refs.galleryMap.insertAdjacentHTML(`beforeend`, templGallery(hits));
+  const last = refs.galleryMap.lastElementChild;
+  last.scrollIntoView({
+    behavior: "smooth",
+    block: "end",
+  });
+}
+function clearInput() {
+  refs.galleryMap.innerHTML = ``;
+}
