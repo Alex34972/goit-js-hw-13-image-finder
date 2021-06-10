5e23dfc1b85663c73e2f60587576b531b94c82a0
Alex34972 <alex_hifi@ukr.net>
2021-06-10 13:12:29 +0300

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,6 +1,6 @@
- const API_KEY = `21948852-6fcb2a22057a93c1128c532ff`;
- const BASE_URL = `https://pixabay.com/api`;
- export default class SearchApiImages {
+const API_KEY = `21948852-6fcb2a22057a93c1128c532ff`;
+const BASE_URL = `https://pixabay.com/api`;
+export default class SearchApiImages {
   constructor() {
     this.searchImages = ``;
     this.page = 1;
@@ -9,10 +9,12 @@
     const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchImages}&page=${this.page}&per_page=12&key=${API_KEY}`;
     return fetch(url)
       .then((responce) => responce.json())
-      .then(({hits}) => {
-        this.page += 1;
+      .then(({ hits }) => {
         return hits;
       });
+  }
+  incrementPage() {
+    this.page += 1;
   }
   resetPage() {
     this.page = 1;
@@ -24,5 +26,3 @@
     this.searchImages = newImages;
   }
 }
-
-
