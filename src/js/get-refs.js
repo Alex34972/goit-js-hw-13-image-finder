c92c0e57dd7c7d6a6fdbe7d19266b304001ab0e4
Alex34972 <alex_hifi@ukr.net>
2021-06-11 20:47:05 +0300

    Update get-refs.js

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -3,5 +3,5 @@ export default function getRefs(){
         searchForm: document.querySelector(".search-form"),
         galleryMap: document.querySelector(".gallery"),
         loadMorBtn: document.querySelector(`[data-action="load-more"]`),
-          
-    }}
+    }
+}
