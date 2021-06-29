11d0cd8fef009f5fd6089898ff18b2be118fc60f
Alex34972 <alex_hifi@ukr.net>
2021-06-29 10:34:58 +0300

    Update fetchImages.js

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -8,7 +8,6 @@ const imagesApiService = new ImagesApiService();
 refs.searchForm.addEventListener(`submit`, onSearch);
 refs.loadMorBtn.addEventListener(`click`, onLoadMore);
 
-
 function onSearch(e) {
   e.preventDefault();
   clearInput();
@@ -16,46 +15,37 @@ function onSearch(e) {
   imagesApiService.resetPage();
   if (imagesApiService.query !== ``) {
     imagesApiService.fetchImages().then(renderGallery);
-  } 
+  }
 }
 function onLoadMore() {
   if (imagesApiService.query !== ``) {
     imagesApiService.incrementPage();
     imagesApiService.fetchImages().then(renderGallery);
-    
-  } 
+  }
 }
 function renderGallery(hits) {
   const last = refs.galleryMap.lastElementChild;
   refs.galleryMap.insertAdjacentHTML(`beforeend`, templGallery(hits));
   onHidden();
 
- if (last!==null) {
-  const next = last.nextElementSibling;
-  next.scrollIntoView({
-  behavior: 'smooth',
-  block: 'start',
-})};
-  if (hits.length<12) {
-  return  refs.loadMorBtn.classList.add(`is-hidden`)
-  } refs.loadMorBtn.classList.remove(`is-hidden`)
+  if (last !== null) {
+    const next = last.nextElementSibling;
+    next.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+  if (hits.length < 12) {
+    return refs.loadMorBtn.classList.add(`is-hidden`);
+  }
+  refs.loadMorBtn.classList.remove(`is-hidden`);
 }
 function clearInput() {
   refs.galleryMap.innerHTML = ``;
-};
-function onHidden () {
-   const cards=document.querySelectorAll(".photo-card");
-  cards.forEach((card) => {card.classList.add(`mapping`)});
-};
-
-
-
-
-//.photo-card.mapping {
-//  opacity: 1;
- // transform: translateX(-50%);
-//  
-// transition: transform var(--animation-duration), opacity var(--timing-function);
-//
-//}
-  
+}
+function onHidden() {
+  const cards = document.querySelectorAll(".photo-card");
+  cards.forEach((card) => {
+    card.classList.add(`mapping`);
+  });
+}
